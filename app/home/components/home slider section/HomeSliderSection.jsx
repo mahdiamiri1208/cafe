a0aca8083c1bd0ab662d222bf5bab56492b14071
mahdiamiri1208 <mahdiamiri1208@gmail.com>
2025-11-18 00:59:13 +0330

    home about and fix bottom of slider and footer

--- app/home/components/home slider section/HomeSliderSection.jsx
+++ app/home/components/home slider section/HomeSliderSection.jsx
@@ -15,20 +15,20 @@ function HomeSliderSection() {
       loop={true}
       navigation={true}
       modules={[Navigation]}
-      className={styles.swiper}
+      className={`${styles.swiper} overlay-bottom`}
     >
       <SwiperSlide
         className={styles.swiper_slide}
         style={{ backgroundImage: `url(${carousel1.src})`}}
       >
         <div
-          class={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+          className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
         >
-          <h2 class="text-primary font-weight-medium m-0">
+          <h2 className="text-primary font-weight-medium m-0">
             We Have Been Serving
           </h2>
-          <h1 class="display-1 text-white m-0">COFFEE</h1>
-          <h2 class="text-white m-0">* SINCE 1950 *</h2>
+          <h1 className="display-1 text-white m-0">COFFEE</h1>
+          <h2 className="text-white m-0">* SINCE 1950 *</h2>
         </div>
       </SwiperSlide>
 
@@ -37,13 +37,13 @@ function HomeSliderSection() {
         style={{ backgroundImage: `url(${carousel2.src})`}}
       >
         <div
-          class={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+          className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
         >
-          <h2 class="text-primary font-weight-medium m-0">
+          <h2 className="text-primary font-weight-medium m-0">
             We Have Been Serving
           </h2>
-          <h1 class="display-1 text-white m-0">COFFEE</h1>
-          <h2 class="text-white m-0">* SINCE 1950 *</h2>
+          <h1 className="display-1 text-white m-0">COFFEE</h1>
+          <h2 className="text-white m-0">* SINCE 1950 *</h2>
         </div>
       </SwiperSlide>
     </Swiper>
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "./home/components/home slider section/HomeSliderSection";
+import HomeAboutSection from "./home/components/home about section/HomeAboutSection";
 
 function page() {
   return (
     <>
       <Slider />
+      <HomeAboutSection />
     </>
   );
 }
