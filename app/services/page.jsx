fac9c3aafd9dadba9d6c0a97b42d2eb45b14f6c3
mahdiamiri1208 <mahdiamiri1208@gmail.com>
2025-11-25 02:28:46 +0330

    services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../components/header/Header";
+import Services from "../components/our services section/Services";
 
 function page() {
-  return <Header />;
+  return (
+    <>
+      <Header route="Services"/>
+      <Services />
+    </>
+  );
 }
 
 export default page;
