12e7f9cd4a04d78ac437a72f9bbab6f9729b0f24
mahdiamiri1208 <mahdiamiri1208@gmail.com>
2025-11-25 18:48:41 +0330

    global search

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+"use client";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
+  const route = useRouter();
+  const [search, setSearch] = useState("");
+
+  const searchHandlerWithEnter = (event) => {
+    if (event.keyCode === 13) {
+      if (search.trim()) {
+        route.push(`/search?q=${search}`);
+      }
+    }
+  };
+
+  const searchHandler = () => {
+    if (search.trim()) {
+      route.push(`/search?q=${search}`);
+    }
+  };
   return (
     <div className={`container-fluid p-0 ${styles.nav_bar}`}>
       <nav
@@ -13,6 +31,34 @@ function Navbar() {
             Next-Coffee
           </h1>
         </Link>
+
+        <div
+          className={styles.searchbox}
+          style={{ position: "relative", display: "inline-block" }}
+        >
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={searchHandlerWithEnter}
+            type="text"
+            className={styles.search_input}
+            placeholder="Search..."
+            style={{ paddingRight: "40px" }}
+          />
+          <i
+            onClick={searchHandler}
+            className="fa-solid fa-magnifying-glass"
+            style={{
+              position: "absolute",
+              right: "10px",
+              top: "50%",
+              transform: "translateY(-50%)",
+              cursor: "pointer",
+              color: "white",
+              paddingBottom: "13px",
+            }}
+          ></i>
+        </div>
 
         <button
           type="button"
---
+++ app/search/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header/Header";
+
+function page() {
+  return (
+    <>
+      <Header route="Search" />
+    </>
+  );
+}
+
+export default page;
